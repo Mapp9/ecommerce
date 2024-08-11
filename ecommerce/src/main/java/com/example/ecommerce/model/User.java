8a73de6b779559c457754e6dbd7354ce10c20c43
Mapp9 <mpp.adso@gmail.com>
2024-08-10 22:31:00 -0500

    Adds user entity, and frontend initial config

--- ecommerce/src/main/java/com/example/ecommerce/model/User.java
+++ ecommerce/src/main/java/com/example/ecommerce/model/User.java
@@ -1,17 +1,23 @@
 package com.example.ecommerce.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-@Entity
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable=false, unique=true)
     private String username;
+    @Column(nullable=false)
     private String password;
+    @Column(nullable=false, unique=true)
     private String email;
 
     // Getters and Setters
